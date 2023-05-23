a050a11d69dfb777efb97189fd9ff7599946b2a4
Martin Rechsteiner <marrechsteiner@gmail.com>
2023-05-23 21:24:38 +0200

    Add custom style for indicator view when using SwiftUI
    This allows you to define a custom PagingIndicatorStyle which can
    contains any type of SwiftUI view that you prefer.

---
+++ Parchment/Protocols/PagingIndicatorStyle.swift
@@ -0,0 +1,53 @@
+import Foundation
+import SwiftUI
+
+@available(iOS 14.0, *)
+public protocol PagingIndicatorStyle {
+    associatedtype Body: View
+    typealias Configuration = PagingIndicatorConfiguration
+    @ViewBuilder func makeBody(configuration: Configuration) -> Body
+}
+
+@available(iOS 14.0, *)
+struct PagingIndicator: View {
+    let configuration: PagingIndicatorConfiguration
+
+    @Environment(\.indicatorStyle) var style
+
+    var body: some View {
+        AnyView(style.makeBody(configuration: configuration))
+    }
+}
+
+@available(iOS 14.0, *)
+public struct PagingIndicatorConfiguration {
+    public let backgroundColor: Color
+}
+
+@available(iOS 14.0, *)
+struct DefaultPagingIndicatorStyle: PagingIndicatorStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Rectangle()
+            .fill(configuration.backgroundColor)
+    }
+}
+
+@available(iOS 14.0, *)
+struct PagingIndicatorStyleKey: EnvironmentKey {
+    static var defaultValue: any PagingIndicatorStyle = DefaultPagingIndicatorStyle()
+}
+
+@available(iOS 14.0, *)
+extension EnvironmentValues {
+    var indicatorStyle: any PagingIndicatorStyle {
+        get { self[PagingIndicatorStyleKey.self] }
+        set { self[PagingIndicatorStyleKey.self] = newValue }
+    }
+}
+
+@available(iOS 14.0, *)
+extension View {
+    public func indicatorStyle(_ style: some PagingIndicatorStyle) -> some View {
+        environment(\.indicatorStyle, style)
+    }
+}
